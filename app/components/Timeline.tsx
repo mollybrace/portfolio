092631bf0cb9cf49ebed91bcd0e44386e0263e96
Molly Brace <mollybrace@Mollys-MacBook-Pro.local>
2024-07-18 19:12:02 +0700

    updating experience component

--- app/components/Timeline.tsx
+++ app/components/Timeline.tsx
@@ -18,8 +18,14 @@ const Timeline = () => {
     <div className="timeline-start mb-10 md:text-end">
       <time className="font-mono italic text-3xl">2024</time>
       <div className="text-lg font-black">Freelance Web Developer</div>
-      <div>I work with clients to design, plan and develop responsive web pages.</div>
-    <div className="text-lg font-black">Freelance Yoga Instructor</div>
+      <div>
+        <ul>
+          <li>Designed and developed mobile-friendly, responsive websites that meet clients' needs.</li>
+          <li>Collaborated with clients to understand their goals and integrated their feedback throughout the development process.</li>
+        </ul>
+
+.</div>
+    <div className="text-lg font-black">Yoga Instructor</div>
       <div className='italic'>January 2024-present</div>
     <div>After studying yoga for one month in Rishikesh, India at the beginning of the year I now work as a freelance Yoga Intructor. Offering holistic sessions both online and in person.</div>
     </div>
@@ -37,13 +43,18 @@ const Timeline = () => {
       <div className='text-gray-400'>Northcoders</div>
       <div>January 2023-April 2023</div>
       </div>
-      <div>Full-stack software engineering bootcamp covering the fundamentals of CSS, HTML, JavaScript, Test Driven Development and Agile development practices. </div>
+      <div>• Full-stack software engineering bootcamp covering the fundamentals of CSS, HTML, JavaScript, Test Driven Development and Agile development practices. 
+ </div>
       <div className="text-lg font-black">Second Stewardess</div>
       <div className='flex justify-between italic'>
       <div className='text-gray-400'>Motor Yacht Belongers</div>
       <div>June 2023-October 2023</div>
       </div>
-      <div>Hello</div>
+      <div>
+        <ul>
+          <li>• Second Stew within an interior team of three on a 50M motor yacht around The Mediterranean.</li>
+        </ul>
+      </div>
       
     </div>
     <hr />
@@ -60,7 +71,12 @@ const Timeline = () => {
       <div className='text-gray-400 italic'>Motor Yacht Sonny II</div>
       <div>June 2022-October 2022</div>
       </div>
-      <div>Hello there</div>
+      <div>
+        <ul>
+          <li>
+          • After leaving Thailand I completed joined a yacht based in Spain and travelled around The Meditarranean for the summer
+          </li>
+        </ul></div>
     </div>
     <hr />
   </li>
@@ -76,7 +92,12 @@ const Timeline = () => {
       <div className='text-gray-400'>Engoo</div>
       <div>January 2023-present</div>
       </div>
-      I have established a global student base of 500+ students as an online 1:1 tutor.
+      <ul>
+        <li>
+        •       I have established a global student base of 500+ students as an online English tutor.
+        </li>
+      </ul>
+
     </div>
     
     <hr />
@@ -94,7 +115,7 @@ const Timeline = () => {
       <div className="text-lg font-black">Business Development Manager</div>
       <div className='text-gray-400'>Social Fox</div>
         <ul>
-            <li>During the first Covid lockdown in Thailand I started working for an Australian based digital marketing agency remotely from Thailand.</li>
+            <li>During the first Covid lockdown in Thailand I started working for an Australian based digital marketing startup agency as their first business development team member.</li>
         </ul>
     </div>
   </li>
@@ -118,8 +139,7 @@ const Timeline = () => {
     <div className="timeline-start mb-10 md:text-start">
       <time className="font-mono italic text-3xl ">2014-2018</time>
       <div className="text-lg font-black">University of Brighton</div>
-      <div>Business Management with Economics</div>
-      <div>First class honours</div>
+      <div>First class honours in Business Management with Economics</div>
       <div className="text-lg font-black">Brandnew IO</div>
       <div>Head of Influencer management</div>
       <div>As my placement year I moved to Berlin to work at a digital marketing start up company. After starting as an intern, after 6 months I was promoted to head of my department and continued to work remotely for the company throughout my final year of university.</div>
