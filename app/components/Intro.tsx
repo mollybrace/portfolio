1c32e1229130ee27c21b9668bf52c36349476f0f
Molly Brace <mollybrace@Mollys-MacBook-Pro.local>
2024-08-22 11:30:44 +0100

    updating profile cards

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -4,21 +4,21 @@ const Intro = () => {
   return (
     <div className='flex justify-center'>
 
-    <div className='flex flex-col items-start text-gray-400 text-balance text-2xl pb-6   w-5/6'>
+    <div className='flex flex-col items-start text-gray-400 text-balance text-xl pb-6   w-5/6'>
         <p className='py-2'>Hi, I'm Molly!</p>
         <p className='py-2'>A Freelance Web Developer,</p>
         <p className='py-2 w-full text-wrap'>
-            After travelling and working around the world for 6 years, I decided to explore the world of software engineering back in 2023! After compelting a rigorous bootcamp where I learned the fundamentals of front-end and back-end development. I now continue to sharpen my skills on personal and client projects.
+            After travelling and working around the world for 6 years, I decided to explore the world of software engineering back in 2023. After compelting a rigorous bootcamp where I learned the fundamentals of front-end and back-end development. I now continue to sharpen my skills on personal and client projects.
         </p>
         <p className='py-2 w-full text-wrap'> 
-            I love coding and building things from start to finish. I have worked with small businesses from the planning, design and development phases and enjoy working in detail on the UX/UI to provide my clients, and their customers with an awesome experience. 
+            I love coding and building things from start to finish. I have worked with small businesses from the planning, design and development phases and enjoy working in detail on the UX to provide my clients, and their customers with an awesome experience. 
         </p>
         <p className='py-2 w-full text-wrap'>
-             I also work freelance as an online English Teacher AND as a Yoga Instructor also after gaining my instructor qualification in India this year!
+             I also work freelance as an online English Teacher AND as a Yoga Instructor also after gaining my instructor qualification in India this year.
         </p>
         <div className='flex justify-center text-center align-middle'>
 
-        <p className='py-4'>Scroll down to find out more!</p>
+        <p className='py-4'>Scroll down to find out more...</p>
         </div>
 
     </div>
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -11,7 +11,7 @@ const Projects = () => {
         img: "/mocean.png",
         repo: "https://github.com/mollybrace/mocean-therapy",
         site: "https://www.mocean-therapy.com/",
-        requirements: ["• A contact form", "• A method to book an appointment", "•Links to socials"]
+        requirements: ["• User can contact business", "• User can book an appointment", "• User can access socials"]
     },
     {
         title: "Jenn Evolve",
@@ -20,7 +20,7 @@ const Projects = () => {
         img: "/jenn.png",
         repo: "https://github.com/mollybrace/jenn-evolve",
         site: "https://jenn-evolve.vercel.app/",
-        requirements: ["• A multipage website displaying two business areas", "• A review feature", "• A contact form", "• Links to socials", "•Aligns with the brand"]
+        requirements: ["• A multipage website displaying two business areas.", "• User can read reviews.", "• User can contact business via multiple streams.", "• User can read FAQs.", "User can find information about sessions offered.", "• Aligns with the brand."]
     },
     {
       title: "Flying Pig Lets",
